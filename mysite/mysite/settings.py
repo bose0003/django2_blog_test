e2f8df3f0c3096537775f38839d367733c659a7d
Subhasis Bose <bose0003.ubuntu@gmail.com>
2019-01-09 16:52:04 +0530

    updated time setting and started app blog
    Updated the settings.py file to show local time instead of UTC and added all the files created by the start app command for blog

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -105,7 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+# Changing time zone to local time, ie, Indian Standard Time
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
